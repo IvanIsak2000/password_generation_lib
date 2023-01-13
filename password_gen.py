f98b9f9d4225a9d0b351ff128f8d502a7c136d97
VANECK <79650307+IvanIsak2000@users.noreply.github.com>
2023-01-13 21:10:35 +0300

    Update password_gen.py

--- password_gen.py
+++ password_gen.py
@@ -1,65 +1,28 @@
-import random
-
-
-def none_():
-    final_password = None
-    print(final_password)
-    exit()
-
-
+import string
+import secrets
+ 
+ 
 def password_generation(
-        adding_a_digit,
+        adding_digits,
         adding_letters,
         adding_special_characters,
-        user_password_length):
-    try:
-        if adding_a_digit == 1 or adding_a_digit == 2 and adding_letters == 1 or adding_letters == 2 or adding_special_characters == 1 or adding_special_characters == 2:
-            if user_password_length > 30:
-                user_password_length = 30
-            if user_password_length < 6:
-                user_password_length = 6
-
-            final_password = ""
-            result = ""
-
-            if adding_a_digit == 1:
-                result += "1234567890"
-
-            elif adding_a_digit == 2:
-                pass
-
-            else:
-                none_()
-
-            if adding_letters == 1:
-                result += "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-
-            elif adding_letters == 2:
-                pass
-
-            else:
-                none_()
-
-            if adding_special_characters == 1:
-                result += "!@#$%^&*()_'\\|?.,"
-
-            elif adding_special_characters == 2:
-                pass
-
-            else:
-                none_()
-            additional_choice = ""
-
-            N = 30  # ПРОГОНИМИМ НАШ resultat_all перед основным выбором
-            while N != 0:
-                additional_choice += random.choice(result)
-                N -= 1
-
-            while user_password_length != 0:  # ЦИКЛ ОКОНЧАТЕЛЬНОГО ВЫБОРА
-                final_password += random.choice(additional_choice)
-
-                user_password_length -= 1
-            print(final_password)
-
-    except BaseException:
-        none_()
+        user_password_length) -> bool:
+ 
+    if not any((adding_digits, adding_letters, adding_special_characters)):
+        raise Exception("Error!")
+ 
+    generation_string = ""
+ 
+    if adding_digits:
+        generation_string += string.digits
+ 
+    if adding_letters:
+        generation_string += string.ascii_letters
+ 
+    if adding_special_characters:
+        generation_string += string.punctuation
+ 
+    password = ''.join(secrets.choice(generation_string)
+                       for i in range(user_password_length))
+    return password
+ 
